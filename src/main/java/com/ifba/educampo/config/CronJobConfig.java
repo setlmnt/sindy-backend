69962f866c2e996d2b1c9d41ba57f47d3b73ba3a
Victor Kayk <victorkayk77@gmail.com>
2023-12-14 19:26:42 -0300

    feat: add cron job that update isPaid of all associates that have a monthly fee expired

---
+++ src/main/java/com/ifba/educampo/config/CronJobConfig.java
@@ -0,0 +1,9 @@
+package com.ifba.educampo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class CronJobConfig {
+}
--- src/main/java/com/ifba/educampo/repository/associate/AssociateRepository.java
+++ src/main/java/com/ifba/educampo/repository/associate/AssociateRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface AssociateRepository extends JpaRepository<Associate, Long> { // Interface de repositório para o Associado
     Page<Associate> findAll(Pageable pageable); // Query para listar todos os Associados
 
@@ -29,4 +31,17 @@ public interface AssociateRepository extends JpaRepository<Associate, Long> { //
             "UPDATE Associate a SET a.localOffice = (SELECT l FROM LocalOffice l WHERE l.id = :localOfficeId) WHERE a.id = :associateId"
     )
     void addLocalOfficeToAssociates(@Param("localOfficeId") Long localOfficeId, @Param("associateId") Long associateId);
+
+    @Query(
+            "SELECT DISTINCT a.id FROM Associate a LEFT JOIN MonthlyFee mf ON a.id = mf.associate.id WHERE a.deleted = false AND a.isPaid = true AND mf.deleted = false AND CURRENT_DATE > (" +
+                    "SELECT mf2.finalDate FROM MonthlyFee mf2 JOIN Associate a2 ON mf2.associate.id = a2.id WHERE a2.id = a.id and mf2.deleted = false AND a2.deleted = false ORDER BY mf2.finalDate DESC LIMIT 1" +
+                    ")"
+    )
+    List<Long> findAllAssociatesWithExpiredMonthlyFee(); // Query para listar todos os Associados com a mensalidade vencida
+
+    @Modifying
+    @Query(
+            "UPDATE Associate a SET a.isPaid = :isPaid WHERE a.id IN :ids"
+    )
+    void updateAssociatesIsPaidByIds(List<Long> ids, boolean isPaid);
 }
