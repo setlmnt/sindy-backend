64f7d64fe28c08eea5956a37f2092a760abf55b4
Victor Kayk <victorkayk77@gmail.com>
2024-03-09 16:14:51 -0300

    refactor: simplifing cors config

--- src/main/java/br/com/sindy/core/config/CorsConfig.java
+++ src/main/java/br/com/sindy/core/config/CorsConfig.java
@@ -1,22 +1,15 @@
 package br.com.sindy.core.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
 }
